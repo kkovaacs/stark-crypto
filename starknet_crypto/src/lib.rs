996838c92db300483cb461cf64d3334959fc73e4
Krisztian Kovacs <krisztian@equilibrium.co>
2022-11-03 15:25:15 +0100

    chore(doc): add description for the starknet_crypto crate

--- starknet_crypto/src/lib.rs
+++ starknet_crypto/src/lib.rs
@@ -1,3 +1,10 @@
+//! Rust crate for Starknet crypto primitives.
+//!
+//! Currently implements the finite field, the elliptic curve (via `starknet_curve`) and hash algorithms.
+//!
+//! DISCLAIMER: the implementation is _not_ constant time. Do _not_ use this crate to construct digital
+//! signatures.
+
 #![deny(rust_2018_idioms)]
 
 mod chain;
